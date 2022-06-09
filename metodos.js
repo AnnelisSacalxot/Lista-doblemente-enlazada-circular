2d6e8c24a70d658f0fdd1967cc3187806b54cd1b
Annelís Sacalxot <annejysch@gmail.com>
2022-06-08 21:02:35 -0600

    No se habia llamado el js a html, ahora se muestra de manera correcta en consola y se tenia lagunso errores de metodos que no fueron llamados

--- metodos.js
+++ metodos.js
@@ -1,6 +1,6 @@
 class Nodo {
     constructor(_numCarne){
-        numCarne = this._numCarne
+       this.numCarne = _numCarne
         //apuntando hacia el siguiente elemento
         this.siguiente = null
     }
@@ -19,14 +19,14 @@ class ListaSimple{
     }
 
     mostrarNum(){
-        temporal = this.cabeza
+       var temporalmuestra = this.cabeza
 
         //mientras temporal no sea nulo se seguira mostrando y si no termina el ciclo
         //(ya no hay otro elemento)
-        while (temporal =! null) {
-            console.log(temporal.numCarne.num)
+        while (temporalmuestra != null) {
+            console.log(temporalmuestra.numCarne.numerito)
             //llamamos al siguiente nodo o numero
-            temporal = temporal.siguiente
+            temporalmuestra = temporalmuestra.siguiente
         }
     }
 
@@ -39,12 +39,30 @@ class ListaSimple{
 
 class numero{
     constructor(_num){
-        this.num = _num
+        this.numerito = _num
     }
 }
 
 var listaNumeroCarne = new ListaSimple();
+var elementoNumero = new numero(2)
+listaNumeroCarne.agregarNum(elementoNumero)
+elementoNumero = new numero("0")
+listaNumeroCarne.agregarNum(elementoNumero)
+elementoNumero = new numero("1")
+listaNumeroCarne.agregarNum(elementoNumero)
+elementoNumero = new numero("7")
+listaNumeroCarne.agregarNum(elementoNumero)
+elementoNumero = new numero("3")
+listaNumeroCarne.agregarNum(elementoNumero)
+elementoNumero = new numero("1")
+listaNumeroCarne.agregarNum(elementoNumero)
+elementoNumero = new numero("5")
+listaNumeroCarne.agregarNum(elementoNumero)
+elementoNumero = new numero("9")
+listaNumeroCarne.agregarNum(elementoNumero)
+elementoNumero = new numero("7")
+listaNumeroCarne.agregarNum(elementoNumero)
 
 
 listaNumeroCarne.mostrarNum();
-listaNumeroCarne.graficarNumeros();
+listaNumeroCarne.graficarNumeros()
