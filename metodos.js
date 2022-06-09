106a4e8aa2ff466542ccc00120c6fbcabefa2681
Annelís Sacalxot <annejysch@gmail.com>
2022-06-09 01:03:15 -0600

    El codigo dot se pinta de manera correcta, se ha creado una lista simple

--- metodos.js
+++ metodos.js
@@ -1,3 +1,14 @@
+/*
+Esto solo para comprobar que se ve el graphviz en el html
+
+d3.select("#lienzo").graphviz()
+.width(900)
+.height(900)
+.renderDot("digraph {a -> b}")
+*/
+
+
+
 class Nodo {
     constructor(_numCarne){
        this.numCarne = _numCarne
@@ -30,9 +41,32 @@ class ListaSimple{
         }
     }
 
-    //IMPORTANTE: se usa sintaxix de Graphviz
+    //IMPORTANTE: se usa sintaxis de Graphviz
     graficarNumeros(){
-
+        var codigodot = "digraph G{\nlabel=\" numero carnet \";\nnode [shape=component];\n";
+        var temporal = this.cabeza
+        var conexiones ="";
+        var nodos ="";
+        var numnodo= 0;
+        while (temporal != null) {
+            nodos+=  "N" + numnodo + "[label=\"" + temporal.numCarne.numerito + "\" ];\n"
+            if(temporal.siguiente != null){
+                var auxnum = numnodo+1
+                conexiones += "N" + numnodo + " -> N" + auxnum + ";\n"
+            }
+            temporal = temporal.siguiente
+            numnodo++;            
+        }
+        codigodot += "//agregando nodos\n"
+        codigodot += nodos+"\n"
+        codigodot += "//agregando conexiones o flechas\n"
+        codigodot += "{rank=same;\n"+conexiones+"\n}\n}"
+        console.log(codigodot)
+        var arreglo = [0, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+        d3.select("#lienzo").graphviz()
+            .width(900)
+            .height(500)
+            .renderDot(codigodot)
     }
 
 }
@@ -44,25 +78,25 @@ class numero{
 }
 
 var listaNumeroCarne = new ListaSimple();
-var elementoNumero = new numero(2)
-listaNumeroCarne.agregarNum(elementoNumero)
-elementoNumero = new numero("0")
-listaNumeroCarne.agregarNum(elementoNumero)
-elementoNumero = new numero("1")
-listaNumeroCarne.agregarNum(elementoNumero)
-elementoNumero = new numero("7")
-listaNumeroCarne.agregarNum(elementoNumero)
-elementoNumero = new numero("3")
-listaNumeroCarne.agregarNum(elementoNumero)
-elementoNumero = new numero("1")
-listaNumeroCarne.agregarNum(elementoNumero)
-elementoNumero = new numero("5")
+var elementoNumero = new numero("7")
 listaNumeroCarne.agregarNum(elementoNumero)
 elementoNumero = new numero("9")
 listaNumeroCarne.agregarNum(elementoNumero)
+elementoNumero = new numero("5")
+listaNumeroCarne.agregarNum(elementoNumero)
+elementoNumero = new numero("1")
+listaNumeroCarne.agregarNum(elementoNumero)
+elementoNumero = new numero("3")
+listaNumeroCarne.agregarNum(elementoNumero)
 elementoNumero = new numero("7")
+listaNumeroCarne.agregarNum(elementoNumero)
+elementoNumero = new numero("1")
+listaNumeroCarne.agregarNum(elementoNumero)
+elementoNumero = new numero("0")
+listaNumeroCarne.agregarNum(elementoNumero)
+elementoNumero = new numero("2")
 listaNumeroCarne.agregarNum(elementoNumero)
 
 
-listaNumeroCarne.mostrarNum();
+listaNumeroCarne.mostrarNum()
 listaNumeroCarne.graficarNumeros()
